cdf7c2282d7d1c5fb769db64dc0703d6c1f16785
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-02-04 22:14:04 -0300

    Add appium

--- app/src/test/java/com/example/testapp/ShowAppTest.kt
+++ app/src/test/java/com/example/testapp/ShowAppTest.kt
@@ -2,8 +2,11 @@ package com.example.testapp
 
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
-import org.junit.Assert
+import io.appium.java_client.functions.ExpectedCondition
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.support.ui.WebDriverWait
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.io.File
@@ -21,6 +24,28 @@ fun removeDuplicate(key: String, s: String): String {
     return key
 }
 
+fun toastMatches(text: String) = ExpectedCondition { driver ->
+    println("searching for toast $text")
+    try {
+        driver?.findElement<WebElement?>(By.xpath("//android.widget.Toast[@text='$text']"))
+            ?.let {
+                println("toast $text appeared")
+                true
+            } ?: {
+            println("toast $text did not appeared")
+            false
+        }()
+    } catch (e: Throwable) {
+        false
+    }
+}
+
+fun waitForToastShown(driver: AndroidDriver<MobileElement>, text: String, waitSeconds: Long = 25) {
+    println("waiting for toast $text")
+    val waitAdLoad = WebDriverWait(driver, waitSeconds)
+    waitAdLoad.until(toastMatches(text))
+}
+
 class ShowAppTest : Spek({
     describe("UI Test") {
         it("should run") {
@@ -29,11 +54,15 @@ class ShowAppTest : Spek({
 
             val capabilities = DesiredCapabilities()
             capabilities.setCapability("app", app.absolutePath)
+            capabilities.setCapability("automationName", "UiAutomator2")
 
             val driver =
                 AndroidDriver<MobileElement>(URL("http://127.0.0.1:4723/wd/hub"), capabilities)
 
-            Thread.sleep(1000)
+            val button = driver.findElementById("fab")
+            button.click()
+
+            waitForToastShown(driver, "AAA")
 
             driver.quit()
         }
